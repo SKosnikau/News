9c6bab71fb587b874ca6b706c9739edf26ad8ef1
Siarhei.Kosnikau <sergeykosnikov@gmail.com>
2022-08-21 13:16:07 +0300

    Updated validation

--- src/main/java/by/htp/ex/util/validation/UserDataValidation.java
+++ src/main/java/by/htp/ex/util/validation/UserDataValidation.java
@@ -6,7 +6,6 @@ import by.htp.ex.service.ServiceException;
 import java.util.List;
 
 public interface UserDataValidation {
-    boolean checkAuthData(String login, String password) throws SecurityException;
+    boolean checkAuthData(String login, String password) throws ServiceException;
     boolean checkRegData(NewUserInfo user) throws ServiceException;
-    public List<String> getErrorsListMessage(String message);
 }
--- src/main/java/by/htp/ex/util/validation/impl/UserDataValidationImpl.java
+++ src/main/java/by/htp/ex/util/validation/impl/UserDataValidationImpl.java
@@ -1,20 +1,19 @@
 package by.htp.ex.util.validation.impl;
 
-import by.htp.ex.bean.ErrorsMessages;
 import by.htp.ex.bean.NewUserInfo;
 import by.htp.ex.service.ServiceException;
 import by.htp.ex.util.validation.UserDataValidation;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.regex.Pattern;
 
 public class UserDataValidationImpl implements UserDataValidation {
-    public List<String> errorsMessagesDataValidation = new ArrayList<>();
     private final String LOG_PAS_CHECK = "[A-Z a-z 0-9]+";
+    private final String EMAIL_CHECK = "\\w+([\\.-]?\\w+)*@\\w+([\\.-]?\\w+)*\\.\\w{2,4}";
+    private final String NAME_AND_SURNAME_CHECK = "[A-Z a-z]+";
+
 
     @Override
-    public boolean checkAuthData(String login, String password) {
+    public boolean checkAuthData(String login, String password) throws ServiceException {
         boolean result = false;
         if (password.isEmpty() || login.isEmpty()) {
             result = false;
@@ -26,7 +25,7 @@ public class UserDataValidationImpl implements UserDataValidation {
     }
 
     @Override
-    public boolean checkRegData(NewUserInfo user) {
+    public boolean checkRegData(NewUserInfo user) throws ServiceException {
         boolean result = false;
         if (nameIsCorrect(user) && surNameIsCorrect(user) && loginIsCorrect(user)
                 && passwordIsCorrect(user) && emailIsCorrect(user)) {
@@ -35,104 +34,59 @@ public class UserDataValidationImpl implements UserDataValidation {
         return result;
     }
 
-    public boolean nameIsCorrect(NewUserInfo user) {
-        boolean result = false;
-        if (!user.getUserName().isEmpty() && nameIsNormal(user)) {
-            result = true;
-        } else {
-            getErrorsListMessage(ErrorsMessages.NAME_ENTERED_INCORRECTLY);
+    private boolean nameIsCorrect(NewUserInfo user) {
+
+        if (user.getUserName().isEmpty() || !nameIsNormal(user)) {
+            return false;
         }
-        return result;
+        return true;
     }
 
-    public boolean nameIsNormal(NewUserInfo user) {
-        boolean result = false;
-        if (Pattern.matches("[A-Z a-z]+", user.getUserName())) {
-            result = true;
+    private boolean nameIsNormal(NewUserInfo user) {
+        return Pattern.matches(NAME_AND_SURNAME_CHECK, user.getUserName());
+    }
+
+    private boolean surNameIsCorrect(NewUserInfo user) {
+        if (user.getUserSurname().isEmpty() || !surNameIsNormal(user)) {
+            return false;
         }
-        return result;
+        return true;
     }
 
-    public boolean surNameIsCorrect(NewUserInfo user) {
-        boolean result = false;
-        if (!user.getUserSurname().isEmpty() && surNameIsNormal(user)) {
-            result = true;
-        } else {
-            getErrorsListMessage(ErrorsMessages.SURNAME_ENTERED_INCORRECTLY);
+    private boolean surNameIsNormal(NewUserInfo user) {
+        return Pattern.matches(NAME_AND_SURNAME_CHECK, user.getUserSurname());
+    }
+
+    private boolean loginIsCorrect(NewUserInfo user) {
+        if (user.getLogin().isEmpty() || !loginIsNormal(user)) {
+            return false;
         }
-        return result;
+        return true;
     }
 
-    public boolean surNameIsNormal(NewUserInfo user) {
-        boolean result = false;
-        if (Pattern.matches("[A-Z a-z]+", user.getUserSurname())) {
-            result = true;
+    private boolean loginIsNormal(NewUserInfo user) {
+        return Pattern.matches(LOG_PAS_CHECK, user.getLogin());
+    }
+
+    private boolean passwordIsCorrect(NewUserInfo user) {
+        if (user.getPassword().isEmpty() || !passwordIsNormal(user)) {
+            return false;
         }
-        return result;
+        return true;
     }
 
-    @Override
-    public List<String> getErrorsListMessage(String message) {
-        errorsMessagesDataValidation.add(message);
-        System.out.println(errorsMessagesDataValidation);
-        return errorsMessagesDataValidation;
+    private boolean passwordIsNormal(NewUserInfo user) {
+        return (Pattern.matches(LOG_PAS_CHECK, user.getPassword()));
     }
 
-    public boolean loginIsCorrect(NewUserInfo user) {
-        boolean result = false;
-        if (!user.getLogin().isEmpty() || loginIsNormal(user)) {
-            result = true;
-        } else {
-            getErrorsListMessage(ErrorsMessages.LOGIN_ENTERED_INCORRECTLY);
+    private boolean emailIsCorrect(NewUserInfo user) {
+        if (user.getEmail().isEmpty() || !emailIsNormal(user)) {
+            return false;
         }
-        return result;
+        return true;
     }
 
-    public boolean loginIsNormal(NewUserInfo user) {
-        boolean result = false;
-        if (Pattern.matches(LOG_PAS_CHECK, user.getLogin())) {
-            result = true;
-        }
-        return result;
+    private boolean emailIsNormal(NewUserInfo user) {
+        return (Pattern.compile(EMAIL_CHECK).matcher(user.getEmail()).matches());
     }
-
-    public boolean passwordIsCorrect(NewUserInfo user) {
-        boolean result = false;
-        if (!user.getPassword().isEmpty() || passwordIsNormal(user)) {
-            result = true;
-        } else {
-            getErrorsListMessage(ErrorsMessages.PASSWORD_NOT_ENTERED);
-        }
-        return result;
-    }
-
-    public boolean passwordIsNormal(NewUserInfo user) {
-        boolean result = false;
-        if (Pattern.matches(LOG_PAS_CHECK, user.getPassword())) {
-            result = true;
-        }
-        return result;
-    }
-
-    public boolean emailIsCorrect(NewUserInfo user) {
-        boolean result = false;
-        if (!user.getEmail().isEmpty() || emailIsNormal(user)) {
-            result = true;
-        } else {
-            getErrorsListMessage(ErrorsMessages.EMAIL_ENTERED_INCORRECTLY);
-        }
-        return result;
-    }
-
-    public boolean emailIsNormal(NewUserInfo user) {
-        boolean result = false;
-        if (Pattern.matches("^[A-Z0-9._%+-]+@[A-Z0-9.-]+\\.[A-Z]{2,6}$", user.getEmail())) {
-            result = true;
-        }
-        return result;
-    }
-
 }
-
-
-
